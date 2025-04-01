d20d25d6a34c72f3b47e9ff819530a832002a3a8
Kevin K H Zheng <kevin.k.h.zheng@qq.com>
2025-04-01 21:43:21 +0800

    init

---
+++ api-auth-server/src/main/java/com/example/api_auth_server/ApiAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAuthServerApplication.class, args);
+	}
+
+}
